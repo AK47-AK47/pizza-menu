b3d3ce57acb6a14b9f78479f77807f9d3eb44237
AK <konstantadam@gmail.com>
2024-09-24 21:53:43 +0300

    fix expand/collapse functionality in accordion menu

--- src/app/pizza-menu/pizza-menu.component.ts
+++ src/app/pizza-menu/pizza-menu.component.ts
@@ -8,10 +8,21 @@ import { Item, Price, Size, items } from '../pizza.data';
   styleUrl: './pizza-menu.component.css',
 })
 export class PizzaMenuComponent {
-  accordionItems = [false, false]; // Array to track open/closed states
+  //accordionItems = [false, false]; // Array to track open/closed states
   pizzaItems: Item[] = items;
+  selectedIndex: number = -1; // Track the currently open index
 
+  /*
   toggleAccordion(index: number) {
     this.accordionItems[index] = !this.accordionItems[index];
+  }*/
+
+  toggleAccordion(index: number) {
+    if (this.selectedIndex === index) {
+      // Close the currently open item if clicked again
+      this.selectedIndex = -1;
+    } else {
+      this.selectedIndex = index;
+    }
   }
 }
